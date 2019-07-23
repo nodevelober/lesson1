8c1fa7519ffb2cb80fcde3bf92efc751e7bb75a6
vladislav <vladlazarevdev@mail.ru>
2019-07-24 00:00:00 +0300

    Initial Commit

---
+++ lesson1task1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson1task1
+//
+//  Created by Владислав Лазарев on 23/07/2019.
+//  Copyright © 2019 Владислав Лазарев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
